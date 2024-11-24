d4f7db80612738aa293a85bb84597cdaa044d207
nibirnafis <nibirnafis@gmail.com>
2024-11-24 23:17:47 +0600

    project complete

--- scripts/retro.js
+++ scripts/retro.js
@@ -1,13 +1,14 @@
 // --------------------On Load-------------------------
 
+
 const defaultPostsLoad = async () => {
     const responce = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
     const data = await responce.json();
     const posts = data.posts
-    defaultPosts(posts)
+    loadPosts(posts)
 }
 
-const defaultPosts = (posts) => {
+const loadPosts = (posts) => {
     const cardsContainer = document.getElementById("cards-container")
 
     posts.forEach(post => {
@@ -21,7 +22,7 @@ const defaultPosts = (posts) => {
             </div>
             <div class="flex flex-col gap-4">
                 <div class="flex font-inter font-medium text-[14px]">
-                    <p class="mr-[20px]"># Music</p>
+                    <p class="mr-[20px]"># ${post.category}</p>
                     <p>Author : ${post.author}</p>
                 </div>
                 <p class="font-mulish font-bold text-[20px]">${post.title}</p>
@@ -36,13 +37,15 @@ const defaultPosts = (posts) => {
                         <img src="Assets/clock_icon.png" alt="">
                         <p class="mx-[25px]">5 min</p>
                     </div>
-                    <div onclick="markedAsRead()">
+                    <button>
                         <img src="Assets/marked-read-icon.png" alt="">
-                    </div>
+                    </button>
                 </div>
             </div>   
         `
         cardsContainer.appendChild(card)
+        // markedAsRead(card)
+        markedAsRead(card, post)
     })
 }
 
@@ -50,7 +53,8 @@ defaultPostsLoad()
 
 
 
-// ---------------------On Typed----------------------
+// ---------------------On searched----------------------
+
 
 const searchPost = () => {
     const cardsContainer = document.getElementById("cards-container")
@@ -63,25 +67,83 @@ const loadSearched = async (typed) => {
     const responce = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${typed}`)
     const data = await responce.json()
     const searchedPosts = data.posts
-    defaultPosts(searchedPosts)
+    loadPosts(searchedPosts)
 }
 
 
 // -----------------marked as read-------------------
 
-const markedAsRead = (event) => {
-    
+const markedAsRead = (card, post) => {
+    document.getElementById(card.id).addEventListener('click', function(){
+        // console.log()
+        const markedReadContainer = document.getElementById('marked-read-container')
 
-    const markedReadContainer = document.getElementById('marked-read-container')
+        const markedReadCard = document.createElement('div')
+        markedReadCard.classList.add('flex', 'justify-between', 'items-center', 'p-[16px]', 'bg-white', 'rounded-[16px]', 'mb-[16px]', 'w-[374px]')
+        markedReadCard.innerHTML = `
+            <p class="font-mulish font-semibold text-[16px]">${post.description}</p>
+            <div class="flex justify-center items-center">
+                <img src="Assets/eye_icon.png" alt="">
+                <p class="font-inter font-normal text-[16px]">1,568</p>
+            </div>
+        `
+        markedReadContainer.appendChild(markedReadCard)
 
-    const card = document.createElement('div')
-    card.classList.add('flex', 'justify-between', 'items-center', 'p-[16px]', 'bg-white', 'rounded-[16px]', 'mb-[16px]')
-    card.innerHTML = `
-        <p class="font-mulish font-semibold text-[16px]">10 Kids Unaware of Their Halloween Costume</p>
-        <div class="flex justify-center items-center">
-            <img src="Assets/eye_icon.png" alt="">
-            <p class="font-inter font-normal text-[16px]">1,568</p>
-        </div>
-    `
-    markedReadContainer.appendChild(card)
+        markReadCounter()
+    })
 }
+
+
+const markReadCounter = () =>{
+    const counter = document.getElementById('counter')
+    const counterText = document.getElementById('counter').innerText
+    let count = parseInt(counterText)
+    count = count + 1
+
+    counter.innerText = count
+    console.log(counter)
+}
+
+
+// --------------------Latest Post---------------------
+
+const latestPosts = async () => {
+    const responce = await fetch("https://openapi.programming-hero.com/api/retro-forum/latest-posts")
+    const posts = await responce.json()
+    // console.log(posts)
+    displayPosts(posts)
+}
+
+const displayPosts = (posts) => {
+    const latestPosts = document.getElementById('latest-posts-container')
+
+    posts.forEach(post => {
+        const latestPostCard = document.createElement('div')
+        latestPostCard.classList.add('w-[350px]', 'p-[24px]', 'border-[1px]', 'rounded-[24px]')
+        latestPostCard.innerHTML = `
+            <div class="mb-[24px]">
+                <img class="w-[326px] h-[190px] rounded-[20px]" src="${post.cover_image}" alt="">
+            </div>
+            <div class="flex mb-[12px]">
+                <div>
+                    <img src="Assets/calender_icon.png" alt="">
+                </div>
+                <p class="font-mulish font-normal text-[16px]">29 January 2024</p>
+            </div>
+            <p class="font-mulish font-extrabold text-[18px] mb-[12px]">${post.title}</p>
+            <p class="font-mulish font-normal text-[16px] mb-[16px]">${post.description}</p>
+            <div class="flex">
+                <div class="mr-[16px]">
+                    <img  class="w-[44px] h-[44px] rounded-[22px]" src="https://i.ibb.co/z8zx95w/pexels-davide-de-giovanni-1649675.jpg" alt="">
+                </div>
+                <div>
+                    <p class="font-mulish font-bold text-[16px]">${post.author.name}</p>
+                    <p class="font-mulish font-normal text-[14px]">${post.author.posted_date}</p>
+                </div>
+            </div>
+        `
+        latestPosts.appendChild(latestPostCard)
+    })
+}
+
+latestPosts()
